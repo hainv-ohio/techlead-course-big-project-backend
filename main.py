783d14ab6314241000096ee6a6d7525a3fc53d30
hainv <hainv@arrowhitech.com>
2022-06-18 09:00:35 +0700

    remove dependencies inject

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
 import sys
 
-from services import user_management
-from dependency_injector import containers, providers
-from dependency_injector.wiring import Provide, inject
-
-from typing import Union
-from fastapi import FastAPI
-
-
-class Container(containers.DeclarativeContainer):
-
-    config = providers.Configuration()
-
-    from core.modules.postgres_module import get_postgres_module
-
-    postgres_module = providers.Singleton(get_postgres_module, use_v2=False)
-
-    # service = providers.Factory(
-    #     Service,
-    #     api_client=api_client,
-    # )
-
-@inject
 def main():
     list_argv = sys.argv
     if len(list_argv) != 2:
