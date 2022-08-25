8912812d339fe7ffa605398efe82934a301a107d
Ryan <ryan.trung@omnyfy.com>
2022-08-25 21:04:30 +0700

    trinhdq: rm kafka

--- services/order_management/di.py
+++ services/order_management/di.py
@@ -2,14 +2,9 @@ from kink import di
 
 from .data.repository.order_repository_impl import OrderRepositoryImpl
 from .domain.repository.order_repository import OrderRepository
-from core.modules.kafka_trinhdq.kafka_impl import KafkaImpl
-from core.modules.kafka_trinhdq.kafka import Kafka
 
 async def init_di():
     order_repository = OrderRepositoryImpl()
-    kafka = KafkaImpl()
     await order_repository.init()
-    await kafka.init()
 
     di[OrderRepository] = order_repository
-    di[Kafka] = kafka
