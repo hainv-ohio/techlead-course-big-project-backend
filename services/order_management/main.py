16e59888d8093e5a0f80c9ff0d419aaea7c732f9
team1 <team@one.com>
2022-10-26 17:06:16 +0000

    vip pro xoxoxo

--- services/order_management/main.py
+++ services/order_management/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv('services/user_management/.env')
 import uvicorn
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,10 +9,8 @@ from .config import cfg
 from .di import init_di
 from .presentation.apis.order_action import router as order_action
 
-app = FastAPI()
-origins = [
-    '*'
-]
+api_root_path = os.getenv('API_ROOT_PATH', '')
+app = FastAPI(root_path=api_root_path)
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,6 +25,6 @@ async def startup_event():
     await init_di()
 
 
-app.include_router(order_action, prefix='/order')
+app.include_router(order_router, prefix='')
 
 uvicorn.run(app, host="0.0.0.0", port=6996)
--- services/user_management/main.py
+++ services/user_management/main.py
@@ -1,4 +1,7 @@
+import os
+
 from dotenv import load_dotenv
+
 load_dotenv('services/user_management/.env')
 
 from fastapi import FastAPI
@@ -9,7 +12,9 @@ import uvicorn
 from .di import init_di
 from .presentation.apis.user_auth import router as user_router
 
-app = FastAPI()
+api_root_path = os.getenv('API_ROOT_PATH', '')
+app = FastAPI(root_path=api_root_path)
+
 origins = [
     '*'
 ]
@@ -27,6 +32,6 @@ async def startup_event():
     await init_di()
 
 
-app.include_router(user_router, prefix='/user')
+app.include_router(user_router, prefix='')
 
 # uvicorn.run(app, host="0.0.0.0", port=6996)
