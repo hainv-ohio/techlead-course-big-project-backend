e4df94333ea2e844ead847bc8f8efb845e8c49a8
Kevin <ngockya98@gmail.com>
2022-10-30 23:38:09 +0700

    update

--- services/item_management/presentation/apis/get_items_by_category_id.py
+++ services/item_management/presentation/apis/get_items_by_category_id.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi import APIRouter, Depends
 
 from ...domain.usecases import *
@@ -9,4 +10,23 @@ getItemsByCategoryRouter = APIRouter()
 async def get_items_by_category_id(id: str,
                          get_items_by_category_id: GetItemsByCategory = Depends(GetItemsByCategory)):
     result = await get_items_by_category_id.execute(id)
-    return result
+    response = {
+        'data': {}
+    }
+    for item in result:
+        item_data = {
+            'id': item.id,
+            'name': item.name,
+            'sku': item.sku,
+            'status': item.status,
+            'category_id': item.category_id,
+            'price': item.price,
+            'currency_code': item.currency_code,
+            'sort_description': item.sort_description,
+            'long_description': item.long_description,
+            'total_sale': item.total_sale,
+            'brand_id': item.brand_id,
+        }
+        response.data.append(item_data)
+
+    return response
